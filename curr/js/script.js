1685b2e681320e7e37c3395985f6304b96ed6591
x15o3i <raphaelfulfilled@gmail.com>
2023-03-17 17:30:17 +0100

    new currency converter

--- curr/js/script.js
+++ curr/js/script.js
@@ -63,6 +63,6 @@ function getExchangeRate(){
         let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
         exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
     }).catch(() =>{ // if user is offline or any other error occured while fetching data then catch function will run
-        exchangeRateTxt.innerText = "Something went wrong";
+        exchangeRateTxt.innerText = "Try again";
     });
 }
