f3dfe9eb7b7c7e5873e97c9a22171c96c8e7fe6d
x15o3i <raphaelfulfilled@gmail.com>
2023-03-20 19:51:21 +0100

    tawk to added

---
+++ new/new.js
@@ -0,0 +1,53 @@
+console.log(40);
+console.log('I\'m angry at Edesiri cause she said i\'m not caring when i\'ve tried my best to be a caring and supporting boyfriend ' + 'she should go to hell for all i care');//and she won't tell me why she said it
+console.log(Math.floor(Math.random() * 51));
+console.log('my baby'.length);
+console.log('Fuck you Edesiri'.toUpperCase());
+console.log('    with this i can remove whitespaces    '.trim());
+var price = Math.floor(Math.random()*100);
+console.log(price);
+let myVar = 'I love her';
+console.log(myVar);
+let her = 'mine';
+her = 'not mine';
+console.log(her);
+/* const can be used instead of let but can be used only once */ const me = 8
+console.log(me)
+let Tea = 425;
+Tea /= 5
+console.log('Power Level:',Tea)
+let favoriteGirl = 'Edesiri'
+console.log('My Favorite girl is ' + favoriteGirl)
+const myWife = 'Edesiri';
+console.log(`My wife is ${myWife}`)
+const girl = 'Edesiri'
+console.log(typeof girl);
+let dog = 'Your bitych ass stinks'
+console.log(dog);
+
+// first work
+const kelvin = 60; //this is for Kelvin
+const celcius = kelvin - 273; // this is to get celcius
+let fahrenheit = celcius * (9 / 5) + 32; // to get fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+console.log('The tempearture is '+ celcius + ' degrees Celcius')
+let newton = celcius * (33 / 10)
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees newton.`)
+
+let sale = 100 > 20
+if (sale){
+  console.log('we here')
+} else {
+  console.log('we not here')
+}
+let powerLevel = 50
+if (powerLevel<=10){
+  console.log('We need to rest')
+} else{
+  console.log('We can keep going')
+}
+
+
+
